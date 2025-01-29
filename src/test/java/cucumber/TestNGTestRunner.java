d075af75c04cbedd72d5bf0b95e1d635b39d0027
Jason Snow <jason@gmail.com>
2025-01-29 08:50:11 -0600

    change to the Runner file

--- src/test/java/cucumber/TestNGTestRunner.java
+++ src/test/java/cucumber/TestNGTestRunner.java
@@ -6,7 +6,7 @@ import io.cucumber.testng.CucumberOptions;
 @CucumberOptions(features="src/test/java/cucumber"
 ,glue="stepDefinition"
 ,monochrome=true
-,tags = "@ErrorValidation"
+,tags = "@Regression"
 ,plugin= {"html:target/cucumber.html"})
 
 public class TestNGTestRunner extends AbstractTestNGCucumberTests{
